fa5dc597c4a55c00b4312f6f27f72b59a0f7e057
shan <nanshanfish@outlook.com>
2026-05-19 01:28:44 +0800

    refactor: rewrite move to trash script to comply with XDG standard
    1. 修复了因展平路径为'%%'引起的可能的文件名过长问题
    2. 垃圾位置遵循 XDG 规范

--- home/fish/dot-config/fish/functions/restore.fish
+++ home/fish/dot-config/fish/functions/restore.fish
@@ -1,24 +1,61 @@
 function restore
-    if test (count $argv) -eq 0
-        set -f restore_fzf_opts "--delimiter=\t" "--with-nth=1" "--preview-window=50%" --preview="__restore_preview $TRASH/{2}" "--multi" --scheme=path
-        set -f items (fd --base-directory="$TRASH" --max-depth=1 | awk '{ orig=$0; gsub(/%%/, "/", $0); print $0 "\t" orig }'| _fzf_wrapper $restore_fzf_opts)
-    else
-        set -f items (echo $argv | awk '{ orig=$0; gsub(/%%/, "/", $0); print $0 "\t" orig }')
-    end
-    set -f total (count $items)
-    set -f cnt 0
+    set -q XDG_DATA_HOME; or set -l XDG_DATA_HOME "$HOME/.local/share"
+    set -l trash_dir "$XDG_DATA_HOME/Trash"
 
-    for item in $items
-        set -l path_and_name (string split \t $item)
-        set -l fpath (echo $path_and_name[1] | string sub -s 12)
-        printf "\r%d/%d:%s" $cnt $total $fpath >&2
-        mkdir -p (path dirname $fpath)
-        if test -e $fpath
-            echo -e "\nERROR: $fpath exists" >&2
+    if not test -d "$trash_dir/info"
+        echo "Trash is empty."
+        return
+    end
+
+    # 1. 解析所有符合 XDG 规范的 .trashinfo 文件，提取原路径
+    set -l fzf_input (
+        for info_file in $trash_dir/info/*.trashinfo
+            if test -f "$info_file"
+                set -l tname (path basename "$info_file" | string replace -r '\.trashinfo$' '')
+                # 正则匹配出 Path= 后面的完整原始路径
+                set -l orig_path (string match -r '^Path=(.*)' (cat "$info_file"))[2]
+                if test -n "$orig_path"
+                    printf "%s\t%s\n" $orig_path $tname
+                end
+            end
+        end
+    )
+
+    if test -z "$fzf_input"
+        echo "Trash is empty."
+        return
+    end
+
+    # 2. 调用 fzf 交互选择
+    if test (count $argv) -eq 0
+        set -l restore_fzf_opts "--delimiter=\t" "--with-nth=1" "--preview-window=50%" --preview="__restore_preview $trash_dir/files/{2}" "--multi" --scheme=path
+        set -f selected (printf "%s\n" $fzf_input | _fzf_wrapper $restore_fzf_opts)
+    else
+        set -f selected (printf "%s\n" $fzf_input | grep -E "$argv")
+    end
+
+    set -l total (count $selected)
+    if test $total -eq 0; return; end
+    set -l cnt 0
+
+    # 3. 还原操作
+    for item in $selected
+        set -l parts (string split \t -- $item)
+        set -l orig_path $parts[1]
+        set -l trash_name $parts[2]
+
+        printf "\r%d/%d:%s" $cnt $total $orig_path >&2
+        mkdir -p (path dirname "$orig_path")
+
+        if test -e "$orig_path"
+            echo -e "\nERROR: $orig_path exists" >&2
         else
-            advmv -g --no-clobber "$TRASH/$path_and_name[2]" $fpath
-            set cnt (math $cnt + 1)
+            # 将 files/ 中的本体移回原位，并销毁 info/ 下的伴生文件
+            if advmv -g --no-clobber "$trash_dir/files/$trash_name" "$orig_path"
+                rm "$trash_dir/info/$trash_name.trashinfo"
+                set cnt (math $cnt + 1)
+            end
         end
     end
-    printf "\r%d/%d" $cnt $total >&2
+    printf "\r%d/%d restored\n" $cnt $total >&2
 end
--- root/clean-trash/usr/local/bin/cleanup-old-files.sh
+++ root/clean-trash/usr/local/bin/cleanup-old-files.sh
@@ -1,24 +1,23 @@
-#!/usr/bin/bash
-
-echo '' > /var/log/clean_old_files.log
+#!/usr/bin/env bash
 
 log() {
     echo "[$(date "+%F %T")] $1"
 }
-exec 3>&1
-exec 2>&1
-exec 1> >(tee -a "/var/log/clean_old_files.log" >&3)
-CLEANUP_DIR=("/home/shan/.local/trash")
 
-for dir in $CLEANUP_DIR; do
-    if test -d $dir; then
-        dir=$(realpath $dir)
-        for item in $(fd --base-directory=$dir --max-depth=1 --changed-before=6months); do
-            log "rm $item..."
-            rm --interactive=never -r "$dir/$item"
-            if test $? -ne 0; then
-                log "Error when rm $dir"
-            fi
-        done
-    fi
-done
+XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+TRASH_DIR="$XDG_DATA_HOME/Trash"
+
+if [ -d "$TRASH_DIR/info" ]; then
+    cd "$TRASH_DIR/info" || exit 1
+
+    fd --max-depth=1 -e trashinfo --changed-before=6months -0 | while IFS= read -r -d '' info_file; do
+        base_name=$(basename "$info_file" .trashinfo)
+        log "[INFO] remove $base_name"
+
+        rm --interactive=never -r "$TRASH_DIR/files/$base_name" "$TRASH_DIR/info/$info_file"
+
+        if [ $? -ne 0 ]; then
+            log "[ERROR] when remove $base_name"
+        fi
+    done
+fi
